8daebdb14c4c1732f0dc0a5df0148fd1e0d5b5f9
OXOYO <zmn2007.hi@163.com>
2017-11-01 18:08:25 +0800

    update.

--- test/appListen.test.js
+++ test/appListen.test.js
@@ -2,20 +2,21 @@
  * Created by OXOYO on 2017/11/1.
  */
 
-var superagent = require('supertest')
+var request = require('supertest')
 var should = require('should')
+// var expect = require('chai').expect
 
 var app = require('../build/index')
 
+// request = request.agent(app.listen())
+
 describe('appListen', function () {
   it('listen success', function (done) {
-    superagent(app.listen())
-      .get('/')
+    request(app.listen())
+      .get('/x-restful-api-generator-koa/')
       .expect(200)
-      .end(function (err, res) {
-        // console.log('err', err)
-        // console.log('res', res)
-        res.status.should.equal(200)
+      .end(function(err, res) {
+        if (err) return done(err)
         done()
       })
   })
