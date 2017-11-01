92a6feb89c29b35bf19a401494db015167547c59
OXOYO <zmn2007.hi@163.com>
2017-11-01 18:08:07 +0800

    add root routers.

--- src/api.js
+++ src/api.js
@@ -13,6 +13,13 @@ import routers from './routers'
 export default function api () {
   let router = new Router({ prefix: ApiConfig.prefix })
   Object.keys(routers).forEach(name => routers[name](router))
+  // 添加根路由
+  router.get('/', async (ctx, next) => {
+    await next()
+    ctx.status = 200
+    ctx.body = {}
+  })
+
   return compose([
     router.routes(),
     router.allowedMethods({
